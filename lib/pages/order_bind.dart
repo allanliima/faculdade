2a2170348cabe7890bf84003f3781ba4e3b3e260
alan.miguel <falm.1987@gmail.com>
2022-10-18 11:30:13 -0300

    flutter aula 2

---
+++ lib/pages/order_bind.dart
@@ -0,0 +1,14 @@
+import 'package:abctechapp/controller/order_controller.dart';
+import 'package:abctechapp/provider/order_provider.dart';
+import 'package:abctechapp/service/geolocation_service.dart';
+import 'package:abctechapp/service/order_service.dart';
+
+import 'package:get/get.dart';
+
+class OrderBind extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<OrderController>(() =>
+        OrderController(GeolocationService(), OrderService(OrderProvider())));
+  }
+}
